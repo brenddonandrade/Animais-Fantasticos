f7629bbcfff02ac1cd708f90c52ada0ae4a30dc9
Brenddon Andrade <brenddonerick@id.uff.br>
2022-03-24 00:33:32 -0300

    :up: terminado o efitod e scroll e finalizado o site

--- script.js
+++ script.js
@@ -66,3 +66,24 @@ function initScrollSuave() {
 }
 
 initScrollSuave();
+
+
+function initAnimationScroll(){
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if(sections){
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0;
+        if (isSectionVisible)
+          section.classList.add('ativo');
+      });
+    }
+    animaScroll();
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+initAnimationScroll();
