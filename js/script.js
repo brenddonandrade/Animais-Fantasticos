f15fd8391374d0108800dae368db71f081ece9b9
Brenddon Andrade <brenddonerick@id.uff.br>
2022-04-15 23:34:42 -0300

    :package: modal

---
+++ js/script.js
@@ -0,0 +1,9 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initAnimationScroll from './modules/scroll-animacao.js';
+import initAccordion from './modules/accordion.js';
+import initTabNav from './modules/tabnav.js';
+
+initScrollSuave();
+initAnimationScroll();
+initAccordion();
+initTabNav();
